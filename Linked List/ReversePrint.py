b28d18332ead119860b3b7c1e1610f0a5ba99a69
Mriganka Gogoi <gogoimriganka09@gmail.com>
2022-09-19 23:30:32 +0530

    Added ReversePrint

---
+++ Linked List/ReversePrint.py
@@ -0,0 +1,44 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+
+
+# Following is the optimized Input for Linked List
+def InputLinkedList():
+    inputList = [int(ele) for ele in input().split()]
+    head = None
+    tail = None
+    for ele in inputList:
+        if ele == -1:
+            break
+        newNode = Node(ele)
+        if head is None:
+            head = newNode
+            tail = newNode
+        else:
+            tail.next = newNode
+            tail = newNode
+    return head 
+
+
+# Print a Linked List
+
+def PrintLL(head):
+    while head is not None:
+        print(str(head.data) + "-->", end='')
+        head = head.next
+    print("None")
+    return
+
+def printReverse(head) :
+    if head is None:
+        return
+    if head.next is None:
+        print(head.data,end=" ")
+        return
+    printReverse(head.next)
+    print(head.data,end=" ")
+node = InputLinkedList()
+PrintLL(node)
+printReverse(node)
