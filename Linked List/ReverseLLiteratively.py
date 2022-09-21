a068b33f4a096f60aa960843d472baf380242341
Mriganka Gogoi <gogoimriganka09@gmail.com>
2022-09-21 18:47:52 +0530

    Midpoint of linked list using fast and slow Method

---
+++ Linked List/ReverseLLiteratively.py
@@ -0,0 +1,50 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+
+
+# Following is the optimized Input for Linked List
+def InputLinkedList():
+    inputList = [int(ele) for ele in input().split()]
+    head = None
+    tail = None
+    for ele in inputList:
+        if ele == -1:
+            break
+        newNode = Node(ele)
+        if head is None:
+            head = newNode
+            tail = newNode
+        else:
+            tail.next = newNode
+            tail = newNode
+    return head 
+
+
+# Print a Linked List
+
+def PrintLL(head):
+    while head is not None:
+        print(str(head.data) + "-->", end='')
+        head = head.next
+    print("None")
+    return
+
+def reverseLLiteratively(head):
+    if head is None or head.next is None:
+        return head
+    
+    prev = None
+    curr = head
+    fwd = None
+    while(curr is not None):
+        fwd = curr.next
+        curr.next = prev
+        prev = curr
+        curr = fwd
+    return prev
+node = InputLinkedList()
+PrintLL(node)
+head = reverseLLiteratively(node)
+PrintLL(head)
