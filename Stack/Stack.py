ce636235d69e5ee78d33debe3a5352b04fa9ee8d
Mriganka Gogoi <gogoimriganka09@gmail.com>
2022-09-28 18:40:10 +0530

    Stack using array

--- Stack/Stack.py
+++ Stack/Stack.py
@@ -7,6 +7,30 @@ class Stack:
         self.__data.append(item)
     
     
-    def pop(self,item):
-        return self.__data.pop(item)
+    def pop(self):
+        if self.isEmpty():
+            return print("Your stack is already empty Babua")
+ 
+        return self.__data.pop()
+
+    def top(self):
+        if self.isEmpty():
+            print("Your Stack is Empty Babua")
+            return
+        return self.__data[len(self.__data) -1]
+
+    def isEmpty(self):
+        return self.size() == 0
+
+    def size(self):
+        return len(self.__data)
+        
+s = Stack()
+s.push(15)
+s.push(21)
+s.push(51)
+while s.isEmpty() is False:
+    print(s.pop())
+
+
     
---
+++ Stack/StackUse.py
@@ -0,0 +1,5 @@
+from Stack import Stack
+
+s =  Stack()
+s.push(5)
+print(s.pop())
